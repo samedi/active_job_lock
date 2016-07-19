61ee7af1ca5f70750daa6a14bbda69477ae24ac7
dferrazm <d.ferrazm@gmail.com>
2016-07-19 15:01:08 +0200

    Comment-out tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,28 +13,28 @@ end
 require 'minitest/pride'
 require 'minitest/autorun'
 
-require 'resque-lock-timeout'
-require dir + '/test_jobs'
-
-# make sure we can run redis-server
-if !system('which redis-server')
-  puts '', "** `redis-server` was not found in your PATH"
-  abort ''
-end
-
-# make sure we can shutdown the server using cli.
-if !system('which redis-cli')
-  puts '', "** `redis-cli` was not found in your PATH"
-  abort ''
-end
-
-puts "Starting redis for testing at localhost:9737..."
-
-# Start redis server for testing.
-`redis-server #{dir}/redis-test.conf`
-Resque.redis = '127.0.0.1:9737'
-
-# After tests are complete, make sure we shutdown redis.
-Minitest.after_run {
-  `redis-cli -p 9737 shutdown nosave`
-}
+# require 'resque-lock-timeout'
+# require dir + '/test_jobs'
+#
+# # make sure we can run redis-server
+# if !system('which redis-server')
+#   puts '', "** `redis-server` was not found in your PATH"
+#   abort ''
+# end
+#
+# # make sure we can shutdown the server using cli.
+# if !system('which redis-cli')
+#   puts '', "** `redis-cli` was not found in your PATH"
+#   abort ''
+# end
+#
+# puts "Starting redis for testing at localhost:9737..."
+#
+# # Start redis server for testing.
+# `redis-server #{dir}/redis-test.conf`
+# Resque.redis = '127.0.0.1:9737'
+#
+# # After tests are complete, make sure we shutdown redis.
+# Minitest.after_run {
+#   `redis-cli -p 9737 shutdown nosave`
+# }
